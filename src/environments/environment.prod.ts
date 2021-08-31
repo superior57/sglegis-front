55719002de7213ce653527a0759664e6136dbabc
Cleiton Marques <cleiton.p.marques@gmail.com>
2021-08-31 18:02:40 -0300

    Ajustes de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,  
-  apiURL: "https://sglegis-api.200.systems/api/v1",
+  apiURL: "https://sglegis.200.systems/api/v1",
   fileURL: "https://sglegis.200.systems"
   
   
